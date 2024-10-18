f05459ad94c160a9dac782d0f6cfa89132a62c5a
benkarting30 <23thompsonben@colchsfc.ac.uk>
2024-10-18 10:43:05 +0100

    Update Quali.js

--- Quali.js
+++ Quali.js
@@ -24,11 +24,11 @@ let carImages = []
 let images = [carImages]
 
 function preload(){
-    carImg1 = loadImage('images/cars/cars_racer (1).png')
-    carImg2 = loadImage('images/cars/cars_racer (2).png')
-    carImg3 = loadImage('images/cars/cars_racer (3).png')
-    carImg4 = loadImage('images/cars/cars_racer (4).png')
-    carImages = [carImg1, carImg2, carImg3, carImg4]
+    // carImg1 = loadImage('images/cars/cars_racer (1).png')
+    // carImg2 = loadImage('images/cars/cars_racer (2).png')
+    // carImg3 = loadImage('images/cars/cars_racer (3).png')
+    // carImg4 = loadImage('images/cars/cars_racer (4).png')
+    // carImages = [carImg1, carImg2, carImg3, carImg4]
 }
 function setup() {
     // Create Canvas and set background and frameRate
@@ -54,7 +54,7 @@ function setup() {
     player.collider = 'd'
     player.tile = 'x'
     player.color = 'yellow'
-    player.image = random(images[0])
+    //player.image = random(images[0])
     player.scale = 0.045
     player.direction = Math.PI / 2
     if (mapSelected == "map2") {
