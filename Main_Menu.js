3334d4d7f09313394cd9442a079b145d86029f70
benkarting30 <23thompsonben@colchsfc.ac.uk>
2024-10-24 09:48:58 +0100

    WE GOT AUDIO

--- Main_Menu.js
+++ Main_Menu.js
@@ -3,14 +3,16 @@ const buttonHeight = 100
 
 let menuStage = 0
 let Music1, Music2
+function preload(){
 
+    Music1 = loadSound('Fast Track.mp3')
+    Music2 = loadSound('Full Throttle.mp3')
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight)
     colorMode(RGB)
     textSize(54)
-    Music1 = loadSound('Fast Track.mp3')
-    Music2 = loadSound('Full Throttle.mp3')
     Music2.play()
 }
 
