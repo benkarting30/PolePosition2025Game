3d56e9a2049734200874def866831b9968d6d374
benkarting30 <23thompsonben@colchsfc.ac.uk>
2024-11-19 13:55:15 +0000

    Removed wild comma

--- Functs.js
+++ Functs.js
@@ -1,20 +1,29 @@
-let laptime = 0
-let fastestLap = null
-export function UpdateData(frames, ){
-    laptime += 1/frames
+// Functs.js
 
-}
-export function GetLaptime(){
-    return laptime
-}
-export function ResetLaptime(){
-    laptime = 0
+let laptime = 0;
+let fastestLap = null;
+
+// UpdateData function: This updates laptime based on the number of frames
+export function UpdateData(frames) {
+    laptime += 1 / frames; // Accumulate laptime
 }
 
-export function SetFL(time){
-    fastestLap = time
+// Get the current laptime
+export function GetLaptime() {
+    return laptime;
 }
 
-export function GetFL(){
-    return fastestLap
+// Reset the laptime to 0
+export function ResetLaptime() {
+    laptime = 0;
+}
+
+// Set the fastest lap time
+export function SetFL(time) {
+    fastestLap = time;
+}
+
+// Get the fastest lap time
+export function GetFL() {
+    return fastestLap;
 }
