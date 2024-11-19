7208ad9caed26949904d70c72e96ffdbf3591d61
benkarting30 <23thompsonben@colchsfc.ac.uk>
2024-11-19 15:15:48 +0000

    more fixes?

--- Functs.js
+++ Functs.js
@@ -1,24 +1,23 @@
 // Functs.js
-window.LapTimeModule = (function(){
-    let laptime = 0;
-    let fastestLap = null;
-    return {
-        GetLaptime: function(){
-            console.log("GetLaptime active");
-            return laptime
-        },
-        ResetLaptime: function(){
-            laptime = 0;
-            console.log("ResetLaptime running") 
-        },
-        UpdateData: function(frames){
-            laptime += 1/frames
-        },
-        SetFL: function(time){
-            fastestLap = time
-        },
-        GetFL: function(){
-            return fastestLap
-        }
-    }   
-})
+window.LapTimeModule = {
+    laptime: 0,
+    fastestLap: null,
+    GetLaptime: function(){
+        console.log("GetLaptime active");
+        return laptime
+    },
+    ResetLaptime: function(){
+        laptime = 0;
+        console.log("ResetLaptime running") 
+    },
+    UpdateData: function(frames){
+        laptime += 1/frames
+    },
+    SetFL: function(time){
+        fastestLap = time
+    },
+    GetFL: function(){
+        return fastestLap
+    }
+  
+}
