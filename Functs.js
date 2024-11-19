348f6fb69c9d234c8d7f1fa93f0cd938e8e75c81
benkarting30 <23thompsonben@colchsfc.ac.uk>
2024-11-19 14:23:26 +0000

    Fix Functs

--- Functs.js
+++ Functs.js
@@ -4,32 +4,32 @@ let laptime = 0;
 let fastestLap = null;
 
 // UpdateData function: This updates laptime based on the number of frames
-export function UpdateData(frames) {
+function UpdateData(frames) {
     laptime += 1 / frames; // Accumulate laptime
     console.log("Update Data runs")
 }
 
 // Get the current laptime
-export function GetLaptime() {
+function GetLaptime() {
     console.log("GetLaptime active")
     return laptime;
 
 }
 
 // Reset the laptime to 0
-export function ResetLaptime() {
+function ResetLaptime() {
     laptime = 0;
     console.log("ResetLaptime running")
 }
 
 // Set the fastest lap time
-export function SetFL(time) {
+function SetFL(time) {
     fastestLap = time;
     console.log("SetFL running")
 }
 
 // Get the fastest lap time
-export function GetFL() {
+function GetFL() {
     console.log("GetFL running")
     return fastestLap;
 }
