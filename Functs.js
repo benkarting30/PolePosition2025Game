a8bb44d718a7013584b1badd53ddd342ca35b0fe
benkarting30 <23thompsonben@colchsfc.ac.uk>
2024-11-26 10:49:02 +0000

    Moron alert

--- Functs.js
+++ Functs.js
@@ -25,21 +25,25 @@ window.LapTimeModule = {
 }
 // Create an anti-cheat function
 window.AntiCheat = function(laptime, fastestLap, speed, sensitivity){
+    console.log(laptime)
+    console.log(fastestLap)
+    console.log(speed)
+    console.log(sensitivity)
     // check whether the state of the fake laptime and fastest lap variables and whether speed and sensitivity are in a certain range
-    if (laptime == 24.231 || fastestLap == 22.342 || speed > 4 || (6>sensitivity || 0<sensitivity)){
+    if (laptime != 24.231 || fastestLap != 22.342 || speed > 4 || (6>sensitivity || 0<sensitivity)){
         // inform the player that they have been caught cheating
         alert("You have attempted to cheat!")
         window.localStorage.setItem("Cheated", true) // Create a item in local storage which informs the code the player cheated upon attempting to resume
         let time = Date.now()
         window.localStorage.setItem("Time", time) // Create a timestamp of when the player was caught
         // Open 7 windows of the youareanidiot on youtube
-        window.open("https://youtu.be/dPtXaAZHuho?si=nxRhBqF30im7HpSI")
-        window.open("https://youtu.be/dPtXaAZHuho?si=nxRhBqF30im7HpSI")
-        window.open("https://youtu.be/dPtXaAZHuho?si=nxRhBqF30im7HpSI")
-        window.open("https://youtu.be/dPtXaAZHuho?si=nxRhBqF30im7HpSI")
-        window.open("https://youtu.be/dPtXaAZHuho?si=nxRhBqF30im7HpSI")
-        window.open("https://youtu.be/dPtXaAZHuho?si=nxRhBqF30im7HpSI")
-        window.location.assign("https://youtu.be/dPtXaAZHuho?si=nxRhBqF30im7HpSI")
+        // window.open("https://youtu.be/dPtXaAZHuho?si=nxRhBqF30im7HpSI")
+        // window.open("https://youtu.be/dPtXaAZHuho?si=nxRhBqF30im7HpSI")
+        // window.open("https://youtu.be/dPtXaAZHuho?si=nxRhBqF30im7HpSI")
+        // window.open("https://youtu.be/dPtXaAZHuho?si=nxRhBqF30im7HpSI")
+        // window.open("https://youtu.be/dPtXaAZHuho?si=nxRhBqF30im7HpSI")
+        // window.open("https://youtu.be/dPtXaAZHuho?si=nxRhBqF30im7HpSI")
+        // window.location.assign("https://youtu.be/dPtXaAZHuho?si=nxRhBqF30im7HpSI")
     }
 }
 
