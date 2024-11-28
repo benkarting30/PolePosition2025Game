2926ca5afe13d5c14c0666b9ea5f42f073a8679d
benkarting30 <23thompsonben@colchsfc.ac.uk>
2024-11-28 12:44:04 +0000

    We go with gif

--- Main_Menu.js
+++ Main_Menu.js
@@ -2,7 +2,7 @@ const buttonWidth = 900
 const buttonHeight = 100
 
 let menuStage = 0
-let Music1, Music2, controls, RobotRock, ThickOfIt
+let Music1, Music2, controls, RobotRock, flag
 let collisionStat = false
 let dynamicStat = false
 let sensitivityLevel = 3, AILevel = 3, qLength = 2, rLength = 15
@@ -32,6 +32,7 @@ function preload() {
     Music2 = loadSound('FTh [music]')
     RobotRock = loadSound('Audio/Robot Rock')
     controls = loadImage('images/pixil-frame-0 (2).png')
+    flag = loadImage('images/ezgif-1-7a5892843b.gif')
 }
 
 function setup() {
@@ -101,6 +102,7 @@ function draw() {
      * These stages determine what buttons are shown to the player
     */
     background(255)
+    image(flag, 0, 0, width, height)
     if (menuStage === 0) { // The First stage visible to the player is the default stage of the menu
         playButton()
         settingsButton()
