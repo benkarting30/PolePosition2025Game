ae8602f378a16ef07ed0a9efca01ef481692c9d0
benkarting30 <23thompsonben@colchsfc.ac.uk>
2024-12-03 13:59:56 +0000

    Commiting myself

--- Time_Trial.js
+++ Time_Trial.js
@@ -409,8 +409,9 @@ function setup() {
 
     }
 
-
-    try{
+    if (!storage){
+        window.location.assign("Main_Menu.html")
+    }
     // From the storage variable, get the 4th character in that variable and use that to determine the map
     switch (storage[3]) {
         case "1":
@@ -1079,9 +1080,6 @@ function setup() {
             break
         case undefined:
             window.location.assign("Main_Menu.html")
-    }
-    } catch {
-        window.location.assign("Main_Menu.html")
     }
     // Create Tiles from Tile map depending on the input
     enginestart.play()
